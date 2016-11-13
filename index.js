780bd1d78d6668ddab7d5609873e498f23ab2d11
sankark <kulanthaivelu.sankaran@ihg.com>
2016-11-12 19:52:38 -0500

    Add more ops.

--- index.js
+++ index.js
@@ -4,12 +4,29 @@ var app = express();
 app.use(express.static('public'));
 app.use('/vendor', express.static('node_modules'));
 
-app.get('/add/:number1/:number2', function(req, res) {
+app.get('/maths/:number1/:number2', function(req, res) {
   var params = req.params, number1 = parseInt(params.number1 || 0, 10), number2 = parseInt(params.number2 || 0, 10);
   console.log("Received request to add: ", number1, number2);
   res.send({
     status : 'SUCCESS',
-    total : (number1 + number2)
+    results: [
+      {
+        key : 'ADDITION',
+        value: (number1 + number2)
+      },
+      {
+        key : 'SUBTRACTION',
+        value: (number1 - number2)
+      },
+      {
+        key : 'MULTIPLICATION',
+        value: (number1 * number2)
+      },
+      {
+        key : 'DIVISION',
+        value: (number1 / number2).toFixed(2)
+      }
+    ]
   });
 });
 
