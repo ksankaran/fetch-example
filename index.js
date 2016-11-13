1c25a11f303c418affca9b26e10b7566f681da44
sankark <kulanthaivelu.sankaran@ihg.com>
2016-11-12 19:22:50 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+app.use('/vendor', express.static('node_modules'));
+
+app.get('/add/:number1/:number2', function(req, res) {
+  var params = req.params, number1 = parseInt(params.number1 || 0, 10), number2 = parseInt(params.number2 || 0, 10);
+  res.send({
+    status : 'SUCCESS',
+    total : (number1 + number2)
+  });
+});
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000! : http://localhost:3000/index.html')
+});
